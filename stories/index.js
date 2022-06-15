dc0b0f3a97bfc52002d8ff5c421225b4ab77ffc0
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-15 19:15:09 -0400

    create confirm component and add message, onConfirm, onCancel action

--- stories/index.js
+++ stories/index.js
@@ -12,7 +12,7 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
-
+import Confirm from "components/Appointment/Confirm.js";
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -145,3 +145,4 @@ storiesOf("Appointment", module)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
   .add("Show", () => <Show onEdit={action("onEdit")} onDelete={action("onDelete")} />)
+  .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
