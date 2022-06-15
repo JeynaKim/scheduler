fb9faae4b5e293c533ffd56937afcd7e7ecf4d1b
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-14 21:24:14 -0400

    add daylistItemStyle and add className library

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,23 +1,14 @@
 import React from "react";
+import className from "classnames";
 import './DayListItem.scss';
 
 export default function DayListItem(props) {
-  const returnClassName = () => {
-    if (props.selected) {
-      return "day-list__item--selected";
-    } 
-
-    if (props.spots === 0) {
-      return "day-list__item--full";
-    }
-
-    return "day-list__item";
-  }
-
-
+    const daylistItemStyle = className("day-list__item", {
+      "day-list__item--selected": props.selected,
+      "day-list__item--full": props.spots === 0,
+    })
   return (
-  
-    <li className={returnClassName()} onClick={() => props.setDay(props.name)}>
+    <li className={daylistItemStyle} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
