c56b89c723a9598b627505729f276975a8f8e6be
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-14 20:41:06 -0400

    add test cases for DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,8 +2,22 @@ import React from "react";
 import './DayListItem.scss';
 
 export default function DayListItem(props) {
+  const returnClassName = () => {
+    if (props.selected) {
+      return "day-list__item--selected";
+    } 
+
+    if (props.spots === 0) {
+      return "day-list__item--full";
+    }
+
+    return "day-list__item";
+  }
+
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+  
+    <li className={returnClassName()} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
