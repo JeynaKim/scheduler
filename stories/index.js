ed02e6d14589c8900d5075c4eb4f7062607b801c
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-15 19:01:27 -0400

    add Show component and pass onEdit and onDelete action

--- stories/index.js
+++ stories/index.js
@@ -10,7 +10,8 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
-import Header from "components/Appointment/Empty.js";
+import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -143,3 +144,4 @@ storiesOf("Appointment", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show onEdit={action("onEdit")} onDelete={action("onDelete")} />)
