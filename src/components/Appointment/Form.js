01f2a6821d3b1e1d939d01b0399ddc3bcce8f4dc
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-15 20:38:59 -0400

    add a reset and cancel function to the Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,12 +1,21 @@
-import React from "react";
 import React, { useState } from 'react';
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
-
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.conCancel();
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -18,11 +27,12 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}  
-
           />
         </form>
         <InterviewerList 
           interviewers={props.interviewers}
+          value={interviewer}
+          onChange={(event) => setInterviewer(event)}
         />
       </section>
       <section className="appointment__card-right">
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -14,6 +14,7 @@ export default function InterviewerList(props) {
       />
     );
   });
+  
   return (
   <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
