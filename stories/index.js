adf668296ef397d75296004a6949336200114536
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-16 13:23:27 -0400

    add proxy: localhost:8001 to the package.json

--- stories/index.js
+++ stories/index.js
@@ -168,6 +168,6 @@ storiesOf("Appointment", module)
         time="4pm"
         interview={{ student: "Lydia Miller-Jones", interviewer }}
       />
-      <Appointment time="5pm" />
+      <Appointment key="last" time="5pm" />
     </Fragment>
   ))
