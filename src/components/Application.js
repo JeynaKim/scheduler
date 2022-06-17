111fca88a4038cf32b55c2666759329a9b056809
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-16 22:12:42 -0400

    complete Retrieving Appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,47 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from './Appointment';
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -50,41 +12,49 @@ export default function Application(props) {
     appointments: {}
   });
 
-  const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
-
-  const convertedAppointments = Object.values(appointments).map((appointment) => {
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const convertedAppointments = dailyAppointments.map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
-  }) 
-  
-  useEffect(() => {
-    axios.get("/api/days").then(response => setState({ ...state, days: response.data }));
-  }, []);
+  })
+
+  const setDay = day => setState({ ...state, day });
+
+  Promise.all([
+    axios.get("/api/days"),
+    axios.get("/api/appointments"),
+  ]).then(response => {
+    setState(prev => ({
+      ...prev,
+      days: response[0].data,
+      appointments: response[1].data,
+    }))
+  })
 
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-        className="sidebar--centered"
-        src="images/logo.png"
-        alt="Interview Scheduler"
-      />
-      <hr className="sidebar__separator sidebar--centered" />
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
             days={state.days}
             value={state.day}
             setDay={setDay}
           />
-      </nav>
-      <img
-        className="sidebar__lhl sidebar--centered"
-        src="images/lhl.png"
-        alt="Lighthouse Labs"
-      />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
         {convertedAppointments}
+        <Appointment time="5pm" />
       </section>
     </main>
   );
