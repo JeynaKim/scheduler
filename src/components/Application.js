af3cdfc05fca00e1376a46aaa28976dd5d8f46ae
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-18 18:38:01 -0400

    import useVisualMode into the Appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from './Appointment';
@@ -9,8 +10,11 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
+
+  const setDay = day => setState({ ...state, day });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const convertedAppointments = dailyAppointments.map((appointment) => {
@@ -25,48 +29,48 @@ export default function Application(props) {
       />
     );
   });
+  
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then(response => {
+      setState(prev => ({
+        ...prev,
+        days: response[0].data,
+        appointments: response[1].data,
+        interviewers: response[2].data
+      }))
+    }, []);
 
-  const setDay = day => setState({ ...state, day });
-
-  Promise.all([
-    axios.get("/api/days"),
-    axios.get("/api/appointments"),
-    axios.get("/api/interviewers"),
-  ]).then(response => {
-    setState(prev => ({
-      ...prev,
-      days: response[0].data,
-      appointments: response[1].data,
-      interviewers: response[2].data
-    }))
-  })
-
-  return (
-    <main className="layout">
-      <section className="sidebar">
-        <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
-        />
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu">
-          <DayList
-            days={state.days}
-            value={state.day}
-            setDay={setDay}
+    return (
+      <main className="layout">
+        <section className="sidebar">
+          <img
+            className="sidebar--centered"
+            src="images/logo.png"
+            alt="Interview Scheduler"
           />
-        </nav>
-        <img
-          className="sidebar__lhl sidebar--centered"
-          src="images/lhl.png"
-          alt="Lighthouse Labs"
-        />
-      </section>
-      <section className="schedule">
-        {convertedAppointments}
-        <Appointment time="5pm" />
-      </section>
-    </main>
-  );
+          <hr className="sidebar__separator sidebar--centered" />
+          <nav className="sidebar__menu">
+            <DayList
+              days={state.days}
+              value={state.day}
+              setDay={setDay}
+            />
+          </nav>
+          <img
+            className="sidebar__lhl sidebar--centered"
+            src="images/lhl.png"
+            alt="Lighthouse Labs"
+          />
+        </section>
+        <section className="schedule">
+          {convertedAppointments}
+          <Appointment time="5pm" />
+        </section>
+      </main>
+    );
+  })
 }
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,8 +3,13 @@ import './styles.scss';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import useVisualMode from "hooks/useVisualMode";
   
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  
+  
   return (
     <article className="appointment">
       <Header time={props.time} />
