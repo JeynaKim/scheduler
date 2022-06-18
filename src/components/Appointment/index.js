09dbd76fd1c00476f8f10946a8a1f02442aa1d8b
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-18 18:58:19 -0400

    add the form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,12 +8,23 @@ import useVisualMode from "hooks/useVisualMode";
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
+  const CREATE = "CREATE";
   
-  
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interviewer= {props.interview.interviewer} />  : <Empty />}
+
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer} />}
+      {mode === CREATE && <Form
+        interviewers={props.interviewers}
+        onCancel={() => back(EMPTY)} />}
     </article>
   )
-}
+};
