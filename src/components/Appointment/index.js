4eb0f7db29e7aed72ea6678cdefaeb7c177a2aec
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-20 16:33:52 -0400

    add SAVING indicator and  update our Appointment component to show the Status component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,12 +4,14 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "hooks/useVisualMode";
   
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
   
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -20,6 +22,7 @@ export default function Appointment(props) {
       student: name,
       interviewer
     };
+    transition(SAVING);
     props.bookInterview(props.id, interview)
       .then(() => transition(SHOW));
   }
@@ -36,7 +39,7 @@ export default function Appointment(props) {
         interviewers={props.interviewers}
         onCancel={back}
         onSave={save} />}
-      {/* {mode === SAVING && <Status message={"Saving"} />} */}
+      {mode === SAVING && <Status message={"Saving"} />}
     </article>
   )
 };
